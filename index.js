d95523fa7bacc0f8be3263676189f5e4c620870b
mahithsc <mahith.chitrapu@gmail.com>
2022-04-14 10:17:52 -0400

    itterating through each link

--- index.js
+++ index.js
@@ -39,17 +39,26 @@ const teamsID = [
 ];
 
 const initBrowser = async () => {
-    const browser = await puppeteer.launch({headless: false});
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage()
     return [page, browser]
 }
 
-const getData = async () => {
+const getData = async (teamLinks) => {
 
     //navigating to the page
     const [page, browser] = await initBrowser()
-    await page.goto('https://espn.com')
+
+    //going through each of the team links
+    for (let team of teamLinks) {
+        await page.goto(team)
+
+        for (let i = 1; i <= 6; i++) {
+            const name = await page.$eval(`#fittPageContainer > div.StickyContainer > div.page-container.cf > div > div > section > div > section > div.nfl-depth-table > div:nth-child(1) > div > div.flex > div > div.Table__Scroller > table > tbody > tr:nth-child(${i}) > td:nth-child(1) > span > a`, el => el.innerText)
+            await page.$eval(`#fittPageContainer > div.StickyContainer > div.page-container.cf > div > div > section > div > section > div.nfl-depth-table > div:nth-child(1) > div > div.flex > div > div.Table__Scroller > table > tbody > tr:nth-child(${i}) > td:nth-child(1) > span > a`, el => el.click())
+        }
+    }
 }
 
-getData()
+    getData(teamsID)
 
